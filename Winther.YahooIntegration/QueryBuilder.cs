bbcbc200af9804e61a56b35c75fd94358f16881d
mad_b <mad_b@DESKTOP-RT3FPJK>
2017-02-25 15:36:45 +0200

    Add project files.

---
+++ Winther.YahooIntegration/QueryBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace Winther.YahooIntegration
+{
+    public class QueryBuilder
+    {
+        public string Select(string fields, string tableName, string condition)
+        {
+            return $"SELECT {fields} FROM {tableName} WHERE {condition}";
+        }
+    }
+}
---
+++ Winther.YahooIntegration/YahooIntegrationService.cs
@@ -0,0 +1,11 @@
+﻿namespace Winther.YahooIntegration
+{
+    public class YahooIntegrationService
+    {
+        private const string EndpointFormat = "https://https://query.yahooapis.com/v1/public/yql?q={0}";
+
+        public YahooIntegrationService()
+        {
+        }
+    }
+}
