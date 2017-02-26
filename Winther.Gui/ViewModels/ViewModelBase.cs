c8d980c0b962c15e3f1da0004cf8082e526959c5
misikdmitriy <misik.dmitriy@gmail.com>
2017-02-26 23:10:13 +0200

    start implementing MVVM

---
+++ Winther.Gui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Winther.Gui.ViewModels
+{
+    public class ViewModelBase
+    {
+        public delegate void ModelChangeDelegate(string fieldName, object value);
+
+        private event ModelChangeDelegate ModelChanged;
+
+
+        protected virtual void OnModelChanged(string fieldname, object value)
+        {
+            ModelChanged?.Invoke(fieldname, value);
+        }
+    }
+}
