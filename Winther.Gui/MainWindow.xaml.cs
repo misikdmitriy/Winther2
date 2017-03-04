684155d3ee95cd48039f76aed82940197700b1f9
misikdmitriy <misik.dmitriy@gmail.com>
2017-03-04 21:01:11 +0200

    remove binding

--- Winther.Gui/MainWindow.xaml.cs
+++ Winther.Gui/MainWindow.xaml.cs
@@ -12,19 +12,12 @@ namespace Winther.Gui
     /// </summary>
     public partial class MainWindow
     {
-        public OneDayForecastDto OneDayForecastDto { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
 
-            var repo = new CityRepository();
-            var city = Task.Run(async () => await repo.GetAsync("London")).Result;
-
-            var service = new OwmIntegrationService(new OwmEndpoints(), ApplicationKey.AppId);
-            var weatherResponse = Task.Run(async() => await service.GetCurrentWeatherAsync(city.Id));
-
-            OneDayForecastDto = weatherResponse.Result;
+            
         }
     }
 }
---
+++ Winther.Gui/MainWindowPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+using Winther.OWMIntegration;
+using Winther.OWMIntegration.Models;
+using Winther.OWMIntegration.Repositories;
+
+namespace Winther.Gui
+{
+    public class MainWindowPresenter
+    {
+        private readonly CityRepository _cityRepository;
+        private readonly OwmIntegrationService _integrationService;
+
+        public MainWindowPresenter(CityRepository cityRepository, OwmIntegrationService integrationService)
+        {
+            _cityRepository = cityRepository;
+            _integrationService = integrationService;
+        }
+
+        public async Task<OneDayForecastDto> GetForecast()
+        {
+            var city = await _cityRepository.GetAsync("London");
+            
+            var weatherResponse = await _integrationService.GetCurrentWeatherAsync(city.Id);
+
+            return weatherResponse;
+        }
+    }
+}
