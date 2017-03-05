93f43b5c02714bd8d914dd2f87eb02dc81af5e6e
misikdmitriy <misik.dmitriy@gmail.com>
2017-03-05 16:52:11 +0200

    small change

---
+++ Winther.Gui/Presenters/IWindowPresenter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Winther.Gui.Presenters
+{
+    public interface IWindowPresenter<in TModel>
+    {
+        Task UpdateAsync(TModel model);
+    }
+}
---
+++ Winther.Gui/Presenters/MainWindowPresenter.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Winther.OWMIntegration;
+using Winther.OWMIntegration.Models;
+using Winther.OWMIntegration.Repositories;
+
+namespace Winther.Gui.Presenters
+{
+    public class MainWindowPresenter : IWindowPresenter<OneDayForecastDto>
+    {
+        private MainWindow _mainWindow;
+
+        public MainWindowPresenter(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public async Task UpdateAsync(OneDayForecastDto forecast)
+        {
+
+        }
+    }
+}
